ae50c9d5c177b7e505fcfc0dac08b6220a7c8011
myuon <ioi.joi.koi.loi@gmail.com>
2018-07-14 17:44:08 +0900

    feat(viewer): realtime face detection

--- recognizer/src/index.ts
+++ recognizer/src/index.ts
@@ -1,20 +1,59 @@
+//import { FacialParts } from '../../server/src/index';
+
 let getUserMedia = (
-    navigator.getUserMedia ||
-    (navigator as any).webkitGetUserMedia ||
-    (navigator as any).mozGetUserMedia
+  navigator.getUserMedia ||
+  (navigator as any).webkitGetUserMedia ||
+  (navigator as any).mozGetUserMedia
 ).bind(navigator);
 
 let video = document.getElementById('video') as HTMLVideoElement;
 
 getUserMedia({video: true, audio: false}, (stream: any) => {
-    console.log(stream);
-    video.src = URL.createObjectURL(stream);
-    
-    let socket = io('http://localhost:3000');
-    socket.on('connect', () => {
-        let peer = new Peer(socket.ids, { host: 'localhost', port: 3000, path: '/peerjs' });
-    });
+  video.src = URL.createObjectURL(stream);
+  
+  let socket = io('http://localhost:3000');
+
+  socket.on('parts', (parts: any) => {
+	let canvas = document.getElementById('parts-canvas') as HTMLCanvasElement;
+	let context = canvas.getContext('2d');
+
+	context.clearRect(0,0,640,480);
+
+	const connectLines = (arr: {x: number, y:number}[]) => {
+	  context.beginPath();
+	  context.moveTo(arr[0].x, arr[0].y);
+  	  for (let i = 1; i < arr.length; i ++) {
+		context.lineTo(arr[i].x, arr[i].y);
+		context.moveTo(arr[i].x, arr[i].y);
+	  }
+	  context.stroke();
+	};
+
+	connectLines(parts.face);
+	connectLines(parts.eye_left);
+	connectLines(parts.eye_right);
+	connectLines(parts.eyeblow_left);
+	connectLines(parts.eyeblow_right);
+	connectLines(parts.eye_left);
+	connectLines(parts.eye_right);
+	connectLines(parts.nose_bridge);
+	connectLines(parts.nasal_cavity);
+	connectLines(parts.mouth_inner);
+	connectLines(parts.mouth_outer);
+  });
+
+  socket.on('connect', () => {
+	let canvas = document.getElementById('video-canvas') as HTMLCanvasElement;
+	let context = canvas.getContext('2d');
+
+	video.addEventListener('play', () => {
+	  setInterval(() => {
+		context.drawImage(video, 0, 0, 320, 240);
+		socket.emit('img', canvas.toDataURL('image/jpeg', 0.6));
+	  }, 1000);
+	});
+  });
 }, (err: any) => {
-    console.log("*** Error ***");
-    console.log(err);
+  console.log("*** Error ***");
+  console.log(err);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import * as path from 'path';
 import * as http from 'http';
 import * as socketio from 'socket.io';
 import * as express from 'express';
+import { HexBase64BinaryEncoding } from 'crypto';
+const cv = require('opencv4nodejs');
+const fr = require('face-recognition').withCv(cv);
 
 let app = express();
 let server = new http.Server(app);
@@ -35,9 +38,56 @@ app.post('/params', (req, res) => {
   res.send(req.body);
 });
 
+export interface Point {
+  x: number,
+  y: number,
+};
+
+export interface FacialParts {
+  face: Point[],
+  eyeblow_right: Point[],
+  eyeblow_left: Point[],
+  eye_right: Point[],
+  eye_left: Point[],
+  nose_bridge: Point[],
+  nasal_cavity: Point[],
+  mouth_outer: Point[],
+  mouth_inner: Point[]
+};
+
+function createFacialParts(arr: Point[]): FacialParts {
+  return {
+    face: arr.slice(0,17),
+    eyeblow_right: arr.slice(17,22),
+    eyeblow_left: arr.slice(22,27),
+    nose_bridge: arr.slice(27,31),
+    nasal_cavity: arr.slice(31,36),
+    eye_right: arr.slice(36,42),
+    eye_left: arr.slice(42,48),
+    mouth_outer: arr.slice(48,60),
+    mouth_inner: arr.slice(60,68),
+  };
+};
+
 io.on('connection', (socket) => {
   socket_ = socket;
   console.log('websocket connect!');
+
+  socket.on('img', (base64: HexBase64BinaryEncoding) => {
+    let output = base64.replace(/^data:image\/(png|jpeg);base64,/, "");
+    let buffer = Buffer.from(output, 'base64');
+    let cvImg = fr.CvImage(cv.imdecode(buffer));
+    let img = fr.cvImageToImageRGB(cvImg);
+
+    const detector = new fr.FrontalFaceDetector();
+    const predictor = fr.FaceLandmark68Predictor();
+    const faceRects = detector.detect(img);
+
+    if (faceRects.length > 0) {
+      const shapes = predictor.predict(img, faceRects[0]);
+      socket.emit('parts', createFacialParts(shapes.getParts().map(p => { return { x: p.x, y: p.y }; })));
+    }
+  });
 });
 
 server.listen(3000, () => {
